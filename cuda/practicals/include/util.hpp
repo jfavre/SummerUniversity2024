acb0e123ed668277b19db658ac1f45c3589922b6
Jean M. Favre <jfavre@cscs.ch>
2025-11-22 09:26:37 +0100

    missing util.hpp change

--- cuda/practicals/include/util.hpp
+++ cuda/practicals/include/util.hpp
@@ -176,7 +176,7 @@ static size_t read_arg(int argc, char** argv, size_t index, int default_value) {
 template <typename T>
 T* malloc_host(size_t N, T value=T()) {
     T* ptr = (T*)(malloc(N*sizeof(T)));
-    std::fill(ptr, ptr+N, value);
+    //std::fill(ptr, ptr+N, value);
 
     return ptr;
 }
